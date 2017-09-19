b3aeebe9fc742e32bc3569fdd659d5b97426d314
wallacetang <wallacetang@mintmedical.cn>
2017-09-19 17:45:52 +0800

    socket

--- njinx/src/main/java/org/njinx/Bootstrap.java
+++ njinx/src/main/java/org/njinx/Bootstrap.java
@@ -1,10 +1,34 @@
 package org.njinx;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author WallaceTang
  */
 public class Bootstrap {
+    public static final String NJINX_PATH = "C:\\Users\\wallacetang\\tmp";
+    public static final int port = 8060;
+    public static boolean shutdown = false;
+
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("start njinx");
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (!shutdown) {
+                System.out.println("accept");
+                Socket socket = serverSocket.accept();
+                OutputStream out = socket.getOutputStream();
+                DataOutputStream dos = new DataOutputStream(out);
+                dos.writeUTF("njinx");
+                dos.flush();
+                dos.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
